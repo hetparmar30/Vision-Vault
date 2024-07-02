309cf15edac0b032bcbdbcdc1fd6d5e4c2d30f81
hetparmar30 <202151103@iiitvadodara.ac.in>
2024-07-02 20:32:40 +0530

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDHDTPQ8YAyZJMU-Rc1uYf-ox0RZmFiWUM",
+  authDomain: "video-3cdc7.firebaseapp.com",
+  projectId: "video-3cdc7",
+  storageBucket: "video-3cdc7.appspot.com",
+  messagingSenderId: "1046465917245",
+  appId: "1:1046465917245:web:e1789339f56d467f5ed554",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
+
+export default app;
